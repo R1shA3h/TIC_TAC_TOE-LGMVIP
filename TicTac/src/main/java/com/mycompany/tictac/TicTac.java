bed3b2654b14455383800608af0e505910abc4fc
Rishabh <99585764+R1shA3h@users.noreply.github.com>
2023-06-30 13:52:27 +0530

    Add files via upload

---
+++ TicTac/src/main/java/com/mycompany/tictac/TicTac.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tictac;
+
+/**
+ *
+ * @author risha
+ */
+public class TicTac {
+
+    public static void main(String[] args) {
+    }
+}
